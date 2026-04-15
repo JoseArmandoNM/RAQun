ffebb7d6fb646feb3b50dfe2361d8c513e6b2f8d
JoseArmandoNM <armandonoguez113@gmail.com>
2026-04-15 01:06:37 -0600

    ya está la documentación de qmeans y estoy comenzando la de wdbscan

--- RAQun/algorithms/QDBSCAN.py
+++ RAQun/algorithms/QDBSCAN.py
@@ -2,7 +2,7 @@ from RAQun.algorithms.base import Algorithm
 
 class QDBSCAN(Algorithm):
     def __init__(self):
-        print("soy qdbscan")
-    
-    def fit(self, X):
+        """
+        
+        """
         pass
--- RAQun/algorithms/QMeans.py
+++ RAQun/algorithms/QMeans.py
@@ -1,13 +1,50 @@
 from RAQun.algorithms.base import Algorithm
+from RAQun.algorithms.QDBSCAN import QDBSCAN
 import numpy as np
 from numpy.typing import NDArray
 
 class QMeans(Algorithm):
-    def __init__(k: int, maxIters: int) -> None:
+    """
+    Quantum-inspired K-Means clustering algorithm.
+
+    Parameters
+    ----------
+    k : int
+        Number of clusters.
+    max_iters : int
+        Maximum number of iterations.
+    """
+    
+    def __init__(self, k: int, maxIters: int) -> None:
         """
-        
+        Initializes the class variables.
+
+        Parameters
+        ----------
+        k : int
+            Number of clusters.
+        maxIters : int
+            Maximum number of iterations before ending.
         """
         pass
+
+    def fit(self, X: NDArray[np.floating]) -> NDArray[np.floating]:
+        """
+        Fits the model to the data.
+
+        Parameters
+        ----------
+        X : NDArray[np.floating]
+            Data matrix of shape (n_samples, n_features).
+
+        Returns
+        -------
+        NDArray[np.floating]
+            Array of shape (n_samples,) with cluster labels.
+        """
+        pass
+
+
 
 
 # """
