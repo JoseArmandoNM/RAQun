e8d26d40181c31fb3e632417ea07a92fb29a9c5e
Maninsch <armandonoguez113@gmail.com>
2026-05-07 17:55:14 -0600

    ya lo subí

--- scratch.py
+++
@@ -1,5 +0,0 @@
-from raqun.algorithms import QDBSCAN
-import numpy as np
-qdbscan = QDBSCAN(eps=1, minSamples=2)
-labels = qdbscan.fit(np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]]))
-print(labels)
